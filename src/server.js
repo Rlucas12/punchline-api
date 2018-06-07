8be0accae3d37c04173d0272f4ac04c703c166fd
REYNES Lucas <lucasreynes@gmail.com>
2018-06-07 21:49:07 +0200

    Adding BodyParser and cleaning code

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const dotenv = require('dotenv').config()
 const Sequelize = require('sequelize')
 const express = require('express')
-const sequelize = new Sequelize(process.env.DB_HOST)
-const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express()
+const sequelize = new Sequelize(process.env.DB_HOST)
+
+// setting body parser middleware 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// API routes
+app.use('/api/artists', artistRouter);
 
 sequelize
   .authenticate()
@@ -15,4 +21,6 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
-app.listen(3000, () => console.log('App listening on port 3000!'))
+app.listen(3000)
+
+module.exports = app;
